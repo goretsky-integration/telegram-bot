b61c41691d8849eb89de3c0ffdb5ef90e67798b5
usbtypec1 <eldos.baktybekov@gmail.com>
2022-06-28 12:39:41 +0600

    :construction: GI-11 Add keyboards

---
+++ src/keyboards/buttons.py
@@ -0,0 +1,79 @@
+from aiogram.types import InlineKeyboardButton, KeyboardButton
+
+import models
+from utils import callback_data
+
+__all__ = (
+    'UpdateStatisticsButton',
+    'SettingsButton',
+    'StatisticsReportsButton',
+    'ShowStatisticsButton',
+    'ChooseRegionButton',
+    'ReportSettingsButton',
+    'SwitchUnitStatusButton',
+)
+
+
+class StatisticsReportsButton(KeyboardButton):
+
+    def __init__(self):
+        super().__init__(text='📊 Отчёты/Статистика')
+
+
+class SettingsButton(KeyboardButton):
+
+    def __init__(self):
+        super().__init__(text='⚙️ Настройки')
+
+
+class UpdateStatisticsButton(InlineKeyboardButton):
+
+    def __init__(self, report_name: str):
+        super().__init__(
+            text='🔄 Обновить',
+            callback_data=callback_data.show_statistics.new(name=report_name),
+        )
+
+
+class ShowStatisticsButton(InlineKeyboardButton):
+
+    def __init__(self, statistics: models.database.StatisticsReportType):
+        super().__init__(
+            text=statistics.value,
+            callback_data=callback_data.show_statistics.new(name=statistics.name)
+        )
+
+
+class ReportSettingsButton(InlineKeyboardButton):
+
+    def __init__(self, report: models.database.ReportType):
+        super().__init__(
+            text=report.value,
+            callback_data=callback_data.report_settings.new(name=report.name)
+        )
+
+
+class ChooseRegionButton(InlineKeyboardButton):
+
+    def __init__(self, report_type: str, region_id: int, region_name: str):
+        super().__init__(
+            text=region_name,
+            callback_data=callback_data.units_by_region.new(
+                region_id=region_id,
+                report_type=report_type,
+            ),
+        )
+
+
+class SwitchUnitStatusButton(InlineKeyboardButton):
+
+    def __init__(self, report_type: str, region_id: int, unit_id: int, unit_name: str, is_unit_enabled: bool):
+        super().__init__(
+            text=f'{"🟢" if is_unit_enabled else "🔴"} {unit_name}',
+            callback_data=callback_data.switch_unit_status.new(
+                report_type=report_type,
+                unit_id=unit_id,
+                region_id=region_id,
+                is_unit_enabled=int(is_unit_enabled),
+            )
+        )
---
+++ src/keyboards/markups.py
@@ -0,0 +1,67 @@
+from typing import Iterable
+
+from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup
+
+import models
+from . import buttons
+
+__all__ = (
+    'UpdateStatisticsReportMarkup',
+    'MainMenuMarkup',
+    'StatisticsReportsMarkup',
+    'RegionsMarkup',
+    'SettingsMarkup',
+    'UnitsMarkup',
+)
+
+
+class MainMenuMarkup(ReplyKeyboardMarkup):
+
+    def __init__(self):
+        super().__init__(row_width=1, resize_keyboard=True)
+        self.add(
+            buttons.StatisticsReportsButton(),
+            buttons.SettingsButton(),
+        )
+
+
+class UpdateStatisticsReportMarkup(InlineKeyboardMarkup):
+
+    def __init__(self, report_name: str):
+        super().__init__()
+        self.add(
+            buttons.UpdateStatisticsButton(report_name),
+        )
+
+
+class StatisticsReportsMarkup(InlineKeyboardMarkup):
+
+    def __init__(self):
+        super().__init__(row_width=1)
+        self.add(*(buttons.ShowStatisticsButton(statistics_type)
+                   for statistics_type in models.database.StatisticsReportType))
+
+
+class SettingsMarkup(InlineKeyboardMarkup):
+
+    def __init__(self):
+        super().__init__(row_width=1)
+        self.add(*(buttons.ReportSettingsButton(report)
+                   for report in models.database.ReportType))
+
+
+class RegionsMarkup(InlineKeyboardMarkup):
+
+    def __init__(self, report_type: str, regions: Iterable[models.Region]):
+        super().__init__()
+        self.add(*(buttons.ChooseRegionButton(report_type, region.id, region.name)
+                   for region in regions))
+
+
+class UnitsMarkup(InlineKeyboardMarkup):
+
+    def __init__(self, report_type: str, region_id: int, enabled_unit_ids: Iterable[int], all_units: Iterable[models.Unit]):
+        super().__init__(row_width=2)
+        for unit in all_units:
+            is_unit_enabled = unit.id in enabled_unit_ids
+            self.insert(buttons.SwitchUnitStatusButton(report_type, region_id, unit.id, unit.name, is_unit_enabled))
