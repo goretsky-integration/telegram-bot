8bdb523a4f74f9d71365d50cfbaba9dc856f7eff
usbtypec1 <eldos.baktybekov@gmail.com>
2022-06-28 12:38:58 +0600

    :construction: GI-11 Add callback data factory

---
+++ src/utils/callback_data.py
@@ -0,0 +1,14 @@
+from aiogram.utils.callback_data import CallbackData
+
+__all__ = (
+    'show_statistics',
+    'report_settings',
+    'units_by_region',
+    'switch_unit_status',
+)
+
+
+show_statistics = CallbackData('show_statistics', 'name')
+report_settings = CallbackData('report_settings', 'name')
+units_by_region = CallbackData('units_by_region', 'region_id', 'report_type')
+switch_unit_status = CallbackData('switch_unit_status', 'unit_id', 'report_type', 'is_unit_enabled', 'region_id')
