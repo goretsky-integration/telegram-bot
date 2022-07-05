ddc8c525c61ebc8b3fca0228e45ac5616b175458
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-05 21:19:23 +0600

    :construction: Add kitchen statistics API request and models

--- src/models/api.py
+++ src/models/api.py
@@ -4,6 +4,10 @@ __all__ = (
     'UnitRevenueForTodayAndWeekBefore',
     'UnitsRevenueMetadata',
     'UnitsRevenueStatistics',
+    'KitchenStatistics',
+    'Tracking',
+    'ProductSpending',
+    'KitchenStatisticsBatch',
 )
 
 
@@ -24,3 +28,32 @@ class UnitsRevenueStatistics(BaseModel):
     revenues: list[UnitRevenueForTodayAndWeekBefore]
     metadata: UnitsRevenueMetadata
     error_unit_ids: list[int]
+
+
+class KitchenRevenue(BaseModel):
+    per_hour: NonNegativeInt
+    delta_from_week_before: float
+
+
+class ProductSpending(BaseModel):
+    per_hour: NonNegativeInt
+    delta_from_week_before: float
+
+
+class Tracking(BaseModel):
+    postponed: NonNegativeInt
+    in_queue: NonNegativeInt
+    in_work: NonNegativeInt
+
+
+class KitchenStatistics(BaseModel):
+    unit_id: PositiveInt
+    revenue: KitchenRevenue
+    product_spending: ProductSpending
+    average_cooking_time: NonNegativeInt
+    tracking: Tracking
+
+
+class KitchenStatisticsBatch(BaseModel):
+    kitchen_statistics: list[KitchenStatistics]
+    error_unit_ids: list[PositiveInt]
--- src/services/api.py
+++ src/services/api.py
@@ -20,3 +20,22 @@ async def get_revenue_statistics(
     if not response.is_success:
         raise exceptions.DodoAPIError
     return models.UnitsRevenueStatistics.parse_obj(response.json())
+
+
+async def get_kitchen_statistics(
+        unit_ids: Iterable[int],
+        cookies: dict,
+) -> models.KitchenStatisticsBatch:
+    url = f'{app_settings.api_url}/v1/statistics/kitchen'
+    body = {'cookies': cookies, 'unit_ids': unit_ids}
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(url, json=body, timeout=30)
+        except httpx.HTTPError:
+            raise exceptions.DodoAPIError
+
+    if not response.is_success:
+        raise exceptions.DodoAPIError
+
+    return models.KitchenStatisticsBatch.parse_obj(response.json())
