bacd81c7ede7c9041b13e38b733757b3f79ba6fc
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-05 21:40:33 +0600

    :construction: Setup pre-shutdown logic

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,18 @@
 from aiogram import Bot, Dispatcher
 from aiogram.types import ParseMode
 
+import db
 from config import app_settings
 
 __all__ = (
     'bot',
     'dp',
+    'on_shutdown',
 )
 
 bot = Bot(app_settings.bot_token, parse_mode=ParseMode.HTML)
 dp = Dispatcher(bot)
+
+
+async def on_shutdown(dispatcher: Dispatcher):
+    await db.close_redis_connection()
--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from aiogram import executor
 
 import handlers
-from bot import dp
+from bot import dp, on_shutdown
 
 
 def main():
     executor.start_polling(
         dispatcher=dp,
         skip_updates=True,
+        on_shutdown=on_shutdown,
     )
 
 
