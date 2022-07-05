fdff2ee526b2d4b30e412ef7ed637c9782e7afc0
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-05 20:48:18 +0600

    :construction: Add redis to get auth credentials

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,2 +1,3 @@
 from .reports import *
 from .units import *
+from .redis_db import *
---
+++ src/db/redis_db.py
@@ -0,0 +1,23 @@
+import redis.asyncio as redis
+
+from config import app_settings
+
+__all__ = (
+    'get_cookies',
+    'get_access_token',
+    'close_redis_connection',
+)
+
+connection = redis.from_url(app_settings.redis_url, decode_responses=True)
+
+
+async def get_cookies(account_name: str) -> dict:
+    return await connection.hgetall(account_name)
+
+
+async def get_access_token(account_name: str) -> str:
+    return await connection.hget('tokens', account_name)
+
+
+async def close_redis_connection():
+    await connection.close()
