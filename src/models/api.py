b3af826122a6cbd32dbe201c7e6c836f73c75609
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-12 02:46:34 +0600

    :sparkles: Add all reports endpoints

--- src/models/api.py
+++ src/models/api.py
@@ -1,26 +1,28 @@
 import uuid
+from typing import TypedDict
 
-from pydantic import BaseModel, NonNegativeInt, PositiveInt, NonNegativeFloat
+from pydantic import BaseModel, NonNegativeInt, Field
 
 __all__ = (
-    'UnitRevenueForTodayAndWeekBefore',
+    'RevenueForTodayAndWeekBeforeStatistics',
+    'RestaurantOrdersStatistics',
+    'RevenueStatistics',
     'UnitsRevenueMetadata',
-    'UnitsRevenueStatistics',
-    'KitchenStatistics',
-    'Tracking',
-    'ProductSpending',
-    'KitchenStatisticsBatch',
-    'UnitDeliveryStatistics',
+    'UnitBonusSystem',
     'UnitBeingLateCertificatesTodayAndWeekBefore',
-    'SingleUnitBeingLateCertificatesTodayAndWeekBefore',
+    'UnitDeliverySpeed',
+    'UnitKitchenPerformance',
+    'KitchenPerformanceStatistics',
+    'UnitDeliveryPerformance',
+    'DeliveryPerformanceStatistics',
+    'HeatedShelfStatistics',
+    'UnitHeatedShelf',
+    'UnitCouriers',
+    'CouriersStatistics',
+    'KitchenProductionStatistics',
+    'UnitKitchenProduction',
+    'UnitIdAndName',
 )
-
-
-class UnitRevenueForTodayAndWeekBefore(BaseModel):
-    unit_id: PositiveInt
-    today: NonNegativeInt
-    week_before: NonNegativeInt
-    delta_from_week_before: float
 
 
 class UnitsRevenueMetadata(BaseModel):
@@ -29,67 +31,104 @@ class UnitsRevenueMetadata(BaseModel):
     delta_from_week_before: float
 
 
-class UnitsRevenueStatistics(BaseModel):
-    revenues: list[UnitRevenueForTodayAndWeekBefore]
+class RevenueForTodayAndWeekBeforeStatistics(BaseModel):
+    unit_id: int
+    today: int
+    week_before: int
+    delta_from_week_before: float
+
+
+class RevenueStatistics(BaseModel):
+    units: list[RevenueForTodayAndWeekBeforeStatistics]
     metadata: UnitsRevenueMetadata
     error_unit_ids: list[int]
 
 
-class KitchenRevenue(BaseModel):
-    per_hour: NonNegativeInt
-    delta_from_week_before: float
-
-
-class ProductSpending(BaseModel):
-    per_hour: NonNegativeInt
-    delta_from_week_before: float
-
-
-class Tracking(BaseModel):
-    postponed: NonNegativeInt
-    in_queue: NonNegativeInt
-    in_work: NonNegativeInt
-
-
-class KitchenStatistics(BaseModel):
-    unit_id: PositiveInt
-    revenue: KitchenRevenue
-    product_spending: ProductSpending
-    average_cooking_time: NonNegativeInt
-    tracking: Tracking
-
-
-class KitchenStatisticsBatch(BaseModel):
-    kitchen_statistics: list[KitchenStatistics]
-    error_unit_ids: list[PositiveInt]
-
-
-class UnitDeliveryStatistics(BaseModel):
-    unit_id: uuid.UUID
-    unit_name: str
-    average_cooking_time: NonNegativeInt
-    average_delivery_order_fulfillment_time: NonNegativeInt
-    average_heated_shelf_time: NonNegativeInt
-    average_order_trip_time: NonNegativeInt
-    couriers_shifts_duration: NonNegativeInt
-    delivery_orders_count: NonNegativeInt
-    delivery_sales: NonNegativeInt
-    late_orders_count: NonNegativeInt
-    orders_with_courier_app_count: NonNegativeInt
-    trips_count: NonNegativeInt
-    trips_duration: NonNegativeInt
-    orders_for_courier_count_per_hour: NonNegativeFloat
-    delivery_with_courier_app_percent: NonNegativeFloat
-    couriers_workload: NonNegativeFloat
+class RestaurantOrdersStatistics(BaseModel):
+    department: str
+    orders_with_phone_numbers_count: int
+    orders_with_phone_numbers_percentage: int
+    total_orders_count: int
 
 
 class UnitBeingLateCertificatesTodayAndWeekBefore(BaseModel):
-    unit_name: str
-    certificates_today_count: NonNegativeInt
-    certificates_week_before_count: NonNegativeInt
-
-
-class SingleUnitBeingLateCertificatesTodayAndWeekBefore(BaseModel):
     unit_id: int
-    certificates_today_count: NonNegativeInt
-    certificates_week_before_count: NonNegativeInt
+    unit_name: str
+    certificates_today_count: int
+    certificates_week_before_count: int
+
+
+class UnitDeliverySpeed(BaseModel):
+    unit_uuid: uuid.UUID
+    unit_name: str
+    average_cooking_time: int
+    average_delivery_order_fulfillment_time: int
+    average_heated_shelf_time: int
+    average_order_trip_time: int
+
+
+class UnitKitchenPerformance(BaseModel):
+    unit_id: int
+    revenue_per_hour: int
+    revenue_delta_from_week_before: int
+
+
+class KitchenPerformanceStatistics(BaseModel):
+    units: list[UnitKitchenPerformance]
+    error_unit_ids: list[int]
+
+
+class UnitDeliveryPerformance(BaseModel):
+    unit_id: int
+    orders_for_courier_count_per_hour_today: float
+    orders_for_courier_count_per_hour_week_before: float
+    delta_from_week_before: int
+
+
+class DeliveryPerformanceStatistics(BaseModel):
+    units: list[UnitDeliveryPerformance]
+    error_unit_ids: list[int]
+
+
+class UnitHeatedShelf(BaseModel):
+    unit_id: int
+    average_awaiting_time: int
+    awaiting_orders_count: int
+
+
+class HeatedShelfStatistics(BaseModel):
+    units: list[UnitHeatedShelf]
+    error_unit_ids: list[int]
+
+
+class UnitCouriers(BaseModel):
+    unit_id: int
+    in_queue_count: int
+    total_count: int
+
+
+class CouriersStatistics(BaseModel):
+    units: list[UnitCouriers]
+    error_unit_ids: list[int]
+
+
+class UnitKitchenProduction(BaseModel):
+    unit_id: int
+    average_cooking_time: int
+
+
+class KitchenProductionStatistics(BaseModel):
+    units: list[UnitKitchenProduction]
+    error_unit_ids: list[int]
+
+
+class UnitBonusSystem(BaseModel):
+    unit_name: str
+    orders_with_phone_numbers_count: int
+    orders_with_phone_numbers_percent: float
+    total_orders_count: int
+
+
+class UnitIdAndName(TypedDict):
+    id: int
+    name: str
--- src/services/api.py
+++ src/services/api.py
@@ -1,6 +1,6 @@
-import asyncio
+import json
 import uuid
-from typing import Iterable, Sequence
+from typing import Iterable, Sequence, TypeVar, Type, Generic
 
 import httpx
 from pydantic import parse_obj_as
@@ -9,88 +9,98 @@ import models
 from config import app_settings
 from utils import exceptions
 
+RM = TypeVar('RM')
+
+
+async def request_to_api(
+        url: str,
+        method: str,
+        params: dict | None = None,
+        body: dict | None = None,
+) -> list | dict:
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.request(method=method, url=url, params=params, json=body, timeout=30)
+            return response.json()
+        except (httpx.HTTPError, json.JSONDecodeError):
+            raise exceptions.DodoAPIError
+
+
+class RequestByCookiesAndUnitIds(Generic[RM]):
+
+    def __init__(self, url: str, response_model: Type[RM]):
+        self._response_model = response_model
+        self._url = url
+
+    async def request(self, cookies: dict, unit_ids: Iterable[int]) -> RM:
+        body = {'cookies': cookies, 'unit_ids': unit_ids}
+        response_json = await request_to_api(self._url, method='POST', body=body)
+        return self._response_model.parse_obj(response_json)
+
+    def __call__(self, cookies: dict, unit_ids: Iterable[int]):
+        return self.request(cookies, unit_ids)
+
+
+get_kitchen_performance_statistics = RequestByCookiesAndUnitIds(
+    url=f'{app_settings.api_url}/v1/statistics/kitchen/performance',
+    response_model=models.KitchenPerformanceStatistics,
+)
+
+get_kitchen_production_statistics = RequestByCookiesAndUnitIds(
+    url=f'{app_settings.api_url}/v1/statistics/production/kitchen',
+    response_model=models.KitchenProductionStatistics,
+)
+
+get_delivery_performance_statistics = RequestByCookiesAndUnitIds(
+    url=f'{app_settings.api_url}/v1/statistics/delivery/performance',
+    response_model=models.DeliveryPerformanceStatistics,
+)
+
+get_heated_shelf_statistics = RequestByCookiesAndUnitIds(
+    url=f'{app_settings.api_url}/v1/statistics/delivery/heated-shelf',
+    response_model=models.HeatedShelfStatistics,
+)
+
+get_couriers_statistics = RequestByCookiesAndUnitIds(
+    url=f'{app_settings.api_url}/v1/statistics/delivery/couriers',
+    response_model=models.CouriersStatistics,
+)
+
 
 async def get_revenue_statistics(
         unit_ids: Iterable[int] | Sequence[int],
-) -> models.UnitsRevenueStatistics:
+) -> models.RevenueStatistics:
     url = f'{app_settings.api_url}/v1/statistics/revenue'
-    params = {'unit_ids': list(unit_ids)}
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(url, params=params, timeout=15)
-        except httpx.HTTPError:
-            raise exceptions.DodoAPIError
-    if not response.is_success:
-        raise exceptions.DodoAPIError
-    return models.UnitsRevenueStatistics.parse_obj(response.json())
-
-
-async def get_kitchen_statistics(
-        unit_ids: Iterable[int],
-        cookies: dict,
-) -> models.KitchenStatisticsBatch:
-    url = f'{app_settings.api_url}/v1/statistics/kitchen'
-    body = {'cookies': cookies, 'unit_ids': unit_ids}
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post(url, json=body, timeout=30)
-        except httpx.HTTPError:
-            raise exceptions.DodoAPIError
-
-    if not response.is_success:
-        raise exceptions.DodoAPIError
-
-    return models.KitchenStatisticsBatch.parse_obj(response.json())
-
-
-async def get_delivery_statistics(
-        access_token: str,
-        unit_uuids: Iterable[str | uuid.UUID],
-) -> list[models.UnitDeliveryStatistics]:
-    url = f'{app_settings.api_url}/v1/statistics/delivery'
-    params = {'token': access_token, 'unit_uuids': tuple(unit_uuids)}
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(url, params=params, timeout=30)
-        except httpx.HTTPError:
-            raise exceptions.DodoAPIError
-
-    if not response.is_success:
-        raise exceptions.DodoAPIError
-
-    return parse_obj_as(list[models.UnitDeliveryStatistics], response.json())
+    params = {'unit_ids': tuple(unit_ids)}
+    response_json = await request_to_api(url, method='GET', params=params)
+    return models.RevenueStatistics.parse_obj(response_json)
 
 
 async def get_being_late_certificates_statistics(
-        unit_ids: Iterable[int] | Sequence[int], cookies: dict
-) -> models.SingleUnitBeingLateCertificatesTodayAndWeekBefore\
-     | list[models.UnitBeingLateCertificatesTodayAndWeekBefore]:
-    """Get count of given certificates for delivery being late.
-
-    Args:
-        unit_ids: collection of unit ids.
-        cookies: cookies as dict.
-
-    Returns:
-        `models.SingleUnitBeingLateCertificatesTodayAndWeekBefore` if only one unit id is specified in parameters,
-        otherwise returns list of `models.UnitBeingLateCertificatesTodayAndWeekBefore`.
-    """
-    url = f'{app_settings.api_url}/v1/statistics/being-late-certificates/today-and-week-before'
-    body = {'cookies': cookies, 'unit_ids': tuple(unit_ids)}
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post(url, json=body, timeout=30)
-        except httpx.HTTPError:
-            raise exceptions.DodoAPIError
-
-    if not response.is_success:
-        raise exceptions.DodoAPIError
-
-    response_json = response.json()
-    if isinstance(response_json, dict):
-        response_json['unit_id'] = tuple(unit_ids)[0]
-        return models.SingleUnitBeingLateCertificatesTodayAndWeekBefore.parse_obj(response_json)
+        cookies: dict,
+        unit_ids_and_names: Iterable[models.UnitIdAndName],
+) -> list[models.UnitBeingLateCertificatesTodayAndWeekBefore]:
+    url = f'{app_settings.api_url}/v1/statistics/being-late-certificates'
+    body = {'cookies': cookies, 'units': tuple(unit_ids_and_names)}
+    response_json = await request_to_api(url, method='POST', body=body)
     return parse_obj_as(list[models.UnitBeingLateCertificatesTodayAndWeekBefore], response_json)
+
+
+async def get_bonus_system_statistics(
+        cookies: dict,
+        unit_ids_and_names: Iterable[models.UnitIdAndName],
+) -> list[models.UnitBonusSystem]:
+    url = f'{app_settings.api_url}/v1/statistics/bonus-system'
+    body = {'cookies': cookies, 'units': tuple(unit_ids_and_names)}
+    response_json = await request_to_api(url, method='POST', body=body)
+    return parse_obj_as(list[models.UnitBonusSystem], response_json)
+
+
+async def get_delivery_speed_statistics(
+        token: str,
+        unit_uuids: Iterable[uuid.UUID],
+) -> list[models.UnitDeliverySpeed]:
+    url = f'{app_settings.api_url}/v2/statistics/delivery/speed'
+    params = {'token': token, 'unit_uuids': [unit_uuid.hex for unit_uuid in unit_uuids]}
+    response_json = await request_to_api(url, method='GET', params=params)
+    return parse_obj_as(list[models.UnitDeliverySpeed], response_json)
