6eda0b9b8e069bae3920f58e56c7af4d2e23d605
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-13 15:39:48 +0600

    :sparkles: Filters return requested units

--- src/services/filters.py
+++ src/services/filters.py
@@ -3,14 +3,14 @@ from aiogram.types import Message, CallbackQuery
 
 import db
 import models
-from utils import logger
+from utils import logger, convert_models
 
 __all__ = (
-    'EnabledUnitIdsFilter',
+    'UnitIdsRequiredFilter',
 )
 
 
-class EnabledUnitIdsFilter(BoundFilter):
+class UnitIdsRequiredFilter(BoundFilter):
 
     async def check(self, query: Message | CallbackQuery) -> bool | dict:
         logger.debug(f'Statistics report unit ids filter')
@@ -18,4 +18,6 @@ class EnabledUnitIdsFilter(BoundFilter):
         enabled_unit_ids = await db.get_unit_ids_by_report_type_and_chat_id(models.ReportType.STATISTICS.name,
                                                                             message.chat.id)
         if enabled_unit_ids:
-            return {'enabled_unit_ids': enabled_unit_ids}
+            units = await db.get_units()
+            enabled_units = [unit for unit in units if unit.id in enabled_unit_ids]
+            return {'units': convert_models.UnitsConverter(enabled_units)}
