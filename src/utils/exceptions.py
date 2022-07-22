a5d707726498e1724ce82a5de738291ef6023ab1
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-22 17:15:26 +0600

    :sparkles: Add decorator `raise_on_none`
    This decorator raises specified exception
    if function (sync or async) returns None.

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,6 +1,36 @@
+import asyncio
+from typing import Type, Iterable, Any
+
+
 class DodoAPIError(Exception):
     pass
 
 
 class NoneUnitIdsSetUpError(Exception):
     pass
+
+
+class NoCookiesError(Exception):
+    pass
+
+
+class NoTokenError(Exception):
+    pass
+
+
+def raise_on_none(exception: Type[Exception], *exception_args: Iterable[Any]):
+    def wrapper(func):
+        if asyncio.iscoroutinefunction(func):
+            async def inner(*args, **kwargs):
+                response = await func(*args, **kwargs)
+                if response is None:
+                    raise exception(*exception_args)
+                return response
+        else:
+            def inner(*args, **kwargs):
+                response = func(*args, **kwargs)
+                if response is None:
+                    raise exception(*exception_args)
+                return response
+        return inner
+    return wrapper
