7d660d15016c5202417227efed8924c85ec30b46
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-22 18:06:06 +0600

    :recycle: Add errors if redis returns None

--- src/db/redis_db.py
+++ src/db/redis_db.py
@@ -1,7 +1,7 @@
 import redis.asyncio as redis
 
 from config import app_settings
-from utils import logger
+from utils import logger, exceptions
 
 __all__ = (
     'get_cookies',
@@ -12,10 +12,12 @@ __all__ = (
 connection = redis.from_url(app_settings.redis_url, decode_responses=True)
 
 
+@exceptions.raise_on_none(exceptions.NoCookiesError)
 async def get_cookies(account_name: str) -> dict:
     return await connection.hgetall(account_name)
 
 
+@exceptions.raise_on_none(exceptions.NoTokenError)
 async def get_access_token(account_name: str) -> str:
     return await connection.hget('tokens', account_name)
 
