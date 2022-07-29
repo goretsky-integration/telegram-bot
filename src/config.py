9f74a508c02f70635262d01b2786a88379b4318f
eldos <eldos.baktybekov@gmail.com>
2022-07-29 14:10:45 +0600

    :heavy_plus_sign: Use `loguru`

--- src/config.py
+++ src/config.py
@@ -1,19 +1,24 @@
+import pathlib
+
 from dotenv import load_dotenv
 from pydantic import BaseSettings, Field
 
 __all__ = (
     'app_settings',
+    'ROOT_PATH',
+    'LOG_FILE_PATH',
 )
 
 load_dotenv()
+
+ROOT_PATH = pathlib.Path(__file__).parent.parent
+LOG_FILE_PATH = ROOT_PATH / 'logs.log'
 
 
 class AppSettings(BaseSettings):
     bot_token: str = Field(..., env='TELEGRAM_BOT_TOKEN')
     api_url: str = Field(..., env='DODO_API_URL')
     db_api_url: str = Field(..., env='DB_API_URL')
-    mongo_db_url: str = Field(..., env='MONGO_DB_URL')
-    redis_url: str = Field(..., env='REDIS_URL')
     debug: bool = Field(..., env='DEBUG')
 
 
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,11 +1,10 @@
-import logging
+from loguru import logger
 
-from config import app_settings
+from config import app_settings, LOG_FILE_PATH
 
 __all__ = (
     'logger',
 )
 
-log_level = logging.DEBUG if app_settings.debug else logging.INFO
-logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
-logger = logging.getLogger()
+log_level = 'DEBUG' if app_settings.debug else 'INFO'
+logger.add(LOG_FILE_PATH, encoding='utf-8', level=log_level)
