da1f9bf400f51316c10f051ff7efc65f04407899
eldos <eldos.baktybekov@gmail.com>
2022-07-29 20:04:10 +0600

    :recycle: Use repository method in filter

--- src/services/filters.py
+++ src/services/filters.py
@@ -1,9 +1,9 @@
 from aiogram.dispatcher.filters import BoundFilter
 from aiogram.types import Message, CallbackQuery
 
-import db
-import models
-from utils import logger, convert_models
+from repositories import DatabaseRepository
+from utils import logger, convert_models, constants
+from services.dependencies import get_db_client
 
 __all__ = (
     'UnitIdsRequiredFilter',
@@ -15,9 +15,12 @@ class UnitIdsRequiredFilter(BoundFilter):
     async def check(self, query: Message | CallbackQuery) -> bool | dict:
         logger.debug(f'Statistics report unit ids filter')
         message = query.message if isinstance(query, CallbackQuery) else query
-        enabled_unit_ids = await db.get_unit_ids_by_report_type_and_chat_id(models.ReportType.STATISTICS.name,
-                                                                            message.chat.id)
-        if enabled_unit_ids:
+        async with get_db_client() as db:
+            enabled_reports = await db.get_reports(constants.ReportType.STATISTICS.name, message.chat.id)
+            enabled_unit_ids = [unit_id for report in enabled_reports for unit_id in report.unit_ids]
+            logger.debug(enabled_unit_ids)
+            if not enabled_unit_ids:
+                return False
             units = await db.get_units()
             enabled_units = [unit for unit in units if unit.id in enabled_unit_ids]
             return {'units': convert_models.UnitsConverter(enabled_units)}
