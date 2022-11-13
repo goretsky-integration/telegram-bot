5983c7742889cba88f8df066a1d2fe3f686de5f8
usbtypec1 <eldos.baktybekov@gmail.com>
2022-11-13 22:40:01 +0600

    :sparkles: Add type alias for all types of incoming update

--- src/models/type_aliases.py
+++ src/models/type_aliases.py
@@ -1,11 +1,15 @@
 from typing import TypeAlias, TypedDict
 
+from aiogram.types import Message, CallbackQuery, Update
+
 __all__ = (
     'Cookies',
     'UnitIdAndName',
+    'Query',
 )
 
 Cookies: TypeAlias = dict[str, str]
+Query: TypeAlias = Message | CallbackQuery | Update
 
 
 class UnitIdAndName(TypedDict):
