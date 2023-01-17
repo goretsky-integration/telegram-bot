8cd976d26636b3fa2120b8118c5e4b3c8b227b1b
Eldos <eldos.baktybekov@gmail.com>
2023-01-17 11:39:24 +0600

    :recycle: GI-4 Use dedicated DTO to render view

--- src/views/statistics_reports/revenue.py
+++ src/views/statistics_reports/revenue.py
@@ -1,29 +1,26 @@
-from dodolib.models import RevenueStatistics
-
-from services.text_utils import intgaps, humanize_percents
+import models.report_views.revenue as models
+from services.text_utils import intgaps
 from views.base import BaseView
 
 __all__ = ('RevenueStatisticsView',)
 
 
 class RevenueStatisticsView(BaseView):
-    __slots__ = ('__revenue_statistics', '__unit_id_to_name')
+    __slots__ = ('__units', '__total')
 
-    def __init__(self, revenue_statistics: RevenueStatistics, unit_id_to_name: dict[int, str]):
-        self.__revenue_statistics = revenue_statistics
-        self.__unit_id_to_name = unit_id_to_name
+    def __init__(self, revenue_statistics: models.RevenueStatisticsReportViewDTO):
+        self.__total = revenue_statistics.total
+        self.__units = sorted(revenue_statistics.units, reverse=True,
+                              key=lambda unit_statistics: unit_statistics.revenue_today)
 
     def get_text(self) -> str:
         lines = ['<b>Выручка за сегодня</b>']
-        units_revenue = sorted(self.__revenue_statistics.results.units, reverse=True,
-                               key=lambda unit_revenue: unit_revenue.today)
-
-        for unit_revenue in units_revenue:
-            unit_name = self.__unit_id_to_name[unit_revenue.unit_id]
-            lines.append(f'{unit_name} | {intgaps(unit_revenue.today)}'
-                         f' | {humanize_percents(unit_revenue.from_week_before_in_percents)}')
-
-        lines.append(f'<b>Итого: {intgaps(self.__revenue_statistics.results.total.today)}'
-                     f' | {humanize_percents(self.__revenue_statistics.results.total.from_week_before_in_percents)}</b>')
-
+        lines += [
+            f'{unit.unit_name}'
+            f' | {intgaps(unit.revenue_today)}'
+            f' | {unit.from_week_before_in_percents:+}%'
+            for unit in self.__units
+        ]
+        lines.append(f'<b>Итого: {intgaps(self.__total.revenue_today)}'
+                     f' | {self.__total.from_week_before_in_percents:+}%</b>')
         return '\n'.join(lines)
