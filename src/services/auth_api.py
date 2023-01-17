392ed4630214108748dda45eb7e190200d7ba9d1
Eldos <eldos.baktybekov@gmail.com>
2023-01-17 22:40:40 +0600

    :truck: GI-4 Rename import alias

--- src/services/auth_api.py
+++ src/services/auth_api.py
@@ -2,7 +2,8 @@ from typing import Callable
 
 import httpx
 
-import models.api_responses.auth as models
+import models.api_responses.auth as auth_models
+import models.api_responses.database as database_models
 from core import exceptions
 from services.api_responses import decode_response_json_or_raise_error
 
@@ -15,16 +16,16 @@ class AuthAPIService:
     def __init__(self, http_client_factory: Callable[[], httpx.AsyncClient]):
         self._http_client_factory = http_client_factory
 
-    async def get_account_tokens(self, account_name: str) -> models.AccountTokens:
+    async def get_account_tokens(self, account_name: str) -> auth_models.AccountTokens:
         request_query_params = {'account_name': account_name}
         async with self._http_client_factory() as client:
             response = await client.get('/auth/token/', params=request_query_params)
         response_data = decode_response_json_or_raise_error(response)
         if response.status_code != 200:
             raise exceptions.AuthAPIServiceError(f'Could not retrieve tokens of account {account_name}')
-        return models.AccountTokens.parse_obj(response_data)
+        return auth_models.AccountTokens.parse_obj(response_data)
 
-    async def get_account_cookies(self, account_name: str) -> models.AccountCookies:
+    async def get_account_cookies(self, account_name: str) -> auth_models.AccountCookies:
         request_query_params = {'account_name': account_name}
         async with self._http_client_factory() as client:
             response = await client.get('/auth/cookies/', params=request_query_params)
