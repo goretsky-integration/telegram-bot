016f78e00cff0cebebb1a78f4aba9338e4ca5814
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 19:29:44 +0600

    :construction: GI-4 Change int to float casting

--- src/models/api_responses/dodo/delivery_productivity.py
+++ src/models/api_responses/dodo/delivery_productivity.py
@@ -7,6 +7,6 @@ __all__ = ('UnitDeliveryProductivityStatisticsReport',)
 
 class UnitDeliveryProductivityStatisticsReport(BaseModel):
     unit_uuid: UUID
-    orders_per_courier_labour_hour_today: int
-    orders_per_courier_labour_hour_week_before: int
+    orders_per_courier_labour_hour_today: float
+    orders_per_courier_labour_hour_week_before: float
     from_week_before_in_percents: int
--- src/models/report_views/delivery_productivity.py
+++ src/models/report_views/delivery_productivity.py
@@ -6,5 +6,5 @@ __all__ = ('UnitDeliveryProductivityStatisticsViewDTO',)
 @dataclass(frozen=True, slots=True)
 class UnitDeliveryProductivityStatisticsViewDTO:
     unit_name: str
-    orders_per_courier_labour_hour_today: int
+    orders_per_courier_labour_hour_today: float
     from_week_before_in_percents: int
