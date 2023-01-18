1b25c3fbe8b6ef7e62c3d8d365f52c1fa89ee73e
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 20:54:55 +0600

    :recycle: GI-4 Refactor `bonus system` view

--- src/views/statistics_reports/bonus_system.py
+++ src/views/statistics_reports/bonus_system.py
@@ -1,29 +1,20 @@
 from typing import Iterable
 
-from dodolib import models
+import models.report_views.bonus_system as models
 from views.base import BaseView
 
 __all__ = ('BonusSystemStatisticsView',)
 
 
 class BonusSystemStatisticsView(BaseView):
-    __slots__ = ('__units_bonus_system_statistics', '__unit_id_to_name')
+    __slots__ = ('__units',)
 
-    def __init__(self, units_bonus_system_statistics: Iterable[models.UnitBonusSystem],
-                 unit_id_to_name: dict[int, str]):
-        self.__units_bonus_system_statistics = units_bonus_system_statistics
-        self.__unit_id_to_name = unit_id_to_name
+    def __init__(self, units_bonus_system_statistics: Iterable[models.UnitBonusSystemStatisticsViewDTO]):
+        self.__units = sorted(units_bonus_system_statistics,
+                              key=lambda unit: unit.orders_with_phone_numbers_percent,
+                              reverse=True)
 
     def get_text(self) -> str:
         lines = ['<b>Бонусная система</b>']
-        sorted_units_statistics = sorted(
-            self.__units_bonus_system_statistics,
-            key=lambda unit: unit.orders_with_phone_numbers_percent,
-            reverse=True
-        )
-
-        for unit in sorted_units_statistics:
-            unit_name = self.__unit_id_to_name[int(unit.unit_id)]
-            lines.append(f'{unit_name} | {unit.orders_with_phone_numbers_percent}% из 100')
-
+        lines += [f'{unit.unit_name} | {unit.orders_with_phone_numbers_percent}% из 100' for unit in self.__units]
         return '\n'.join(lines)
