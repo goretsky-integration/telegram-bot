a78383609032b8b4d9b3aace3fb122e1ca538e92
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 18:26:02 +0600

    :recycle: GI-4 Refactor `restaurant cooking time` view

--- src/views/statistics_reports/restaurant_cooking_time.py
+++ src/views/statistics_reports/restaurant_cooking_time.py
@@ -1,8 +1,6 @@
-import uuid
 from typing import Iterable
 
-from dodolib import models
-
+import models.report_views.restaurant_cooking_time as models
 from services.text_utils import humanize_seconds
 from views.base import BaseView
 
@@ -10,20 +8,16 @@ __all__ = ('RestaurantCookingTimeStatisticsView',)
 
 
 class RestaurantCookingTimeStatisticsView(BaseView):
-    __slots__ = ('__units_restaurant_cooking_time_statistics', '__unit_uuid_to_name')
+    __slots__ = ('__units',)
 
-    def __init__(self, units_restaurant_cooking_time_statistics: Iterable[models.UnitRestaurantCookingTimeStatistics],
-                 unit_uuid_to_name: dict[uuid.UUID, str]):
-        self.__units_restaurant_cooking_time_statistics = units_restaurant_cooking_time_statistics
-        self.__unit_uuid_to_name = unit_uuid_to_name
+    def __init__(self, restaurant_cooking_time_statistics: Iterable[models.UnitRestaurantCookingTimeStatisticsViewDTO]):
+        self.__units = sorted(restaurant_cooking_time_statistics,
+                              key=lambda unit: unit.average_tracking_pending_and_cooking_time)
 
     def get_text(self) -> str:
         lines = ['<b>Время приготовления в ресторане</b>']
-        sorted_units_statistics = sorted(self.__units_restaurant_cooking_time_statistics,
-                                         key=lambda unit: unit.average_tracking_pending_and_cooking_time)
-
-        for unit in sorted_units_statistics:
-            unit_name = self.__unit_uuid_to_name[unit.unit_uuid]
-            lines.append(f'{unit_name} | {humanize_seconds(unit.average_tracking_pending_and_cooking_time)}')
-
+        lines += [
+            f'{unit.unit_name} | {humanize_seconds(unit.average_tracking_pending_and_cooking_time)}'
+            for unit in self.__units
+        ]
         return '\n'.join(lines)
