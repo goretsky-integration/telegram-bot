33cfe1bddaa10b8a9e51ba9feea2655f5c6a8b18
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 10:52:00 +0600

    :construction: GI-4 Add `kitchen productivity` view dto

--- src/models/report_views/__init__.py
+++ src/models/report_views/__init__.py
@@ -1,2 +1,3 @@
-from .revenue import *
 from .awaiting_orders import *
+from .kitchen_productivity import *
+from .revenue import *
---
+++ src/models/report_views/kitchen_productivity.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Iterable
+
+__all__ = ('UnitKitchenProductivityStatisticsDTO', 'KitchenProductivityStatisticsReportViewDTO')
+
+
+@dataclass(frozen=True, slots=True)
+class UnitKitchenProductivityStatisticsDTO:
+    unit_name: str
+    sales_per_labor_hour_today: 0
+    from_week_before_percent: 0
+    total_cooking_time: 0
+
+
+@dataclass(frozen=True, slots=True)
+class KitchenProductivityStatisticsReportViewDTO:
+    units: Iterable[UnitKitchenProductivityStatisticsDTO]
+    error_unit_names: Iterable[str]
