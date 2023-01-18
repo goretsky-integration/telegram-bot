bb38d183f74e10f353b58bd31c12ef52c8a3377e
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 19:20:34 +0600

    :construction: GI-4 Add `delivery productivity` view DTO

---
+++ src/models/report_views/delivery_productivity.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+__all__ = ('UnitDeliveryProductivityStatisticsViewDTO',)
+
+
+@dataclass(frozen=True, slots=True)
+class UnitDeliveryProductivityStatisticsViewDTO:
+    unit_name: str
+    orders_per_courier_labour_hour_today: int
+    from_week_before_in_percents: int
