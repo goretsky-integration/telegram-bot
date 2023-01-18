ac742161200e9f6f9e94198939ce588f6db32e9d
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 19:29:08 +0600

    :recycle: GI-4 Refactor `delivery productivity` view

--- src/views/statistics_reports/delivery_productivity.py
+++ src/views/statistics_reports/delivery_productivity.py
@@ -1,31 +1,24 @@
-import uuid
 from typing import Iterable
 
-from dodolib import models
-
-from services.text_utils import humanize_percents
+import models.report_views.delivery_productivity as models
 from views.base import BaseView
 
 __all__ = ('DeliveryProductivityStatisticsView',)
 
 
 class DeliveryProductivityStatisticsView(BaseView):
-    __slots__ = ('__units_delivery_productivity_statistics', '__unit_uuid_to_name')
+    __slots__ = ('__units',)
 
-    def __init__(self, units_delivery_productivity_statistics: Iterable[models.UnitDeliveryProductivityStatistics],
-                 unit_uuid_to_name: dict[uuid.UUID, str]):
-        self.__units_delivery_productivity_statistics = units_delivery_productivity_statistics
-        self.__unit_uuid_to_name = unit_uuid_to_name
+    def __init__(self, delivery_productivity_statistics: Iterable[models.UnitDeliveryProductivityStatisticsViewDTO]):
+        self.__units = sorted(delivery_productivity_statistics,
+                              key=lambda unit: unit.orders_per_courier_labour_hour_today,
+                              reverse=True)
 
     def get_text(self) -> str:
         lines = ['<b>Заказов на курьера в час</b>']
-        units_sorted_statistics = sorted(self.__units_delivery_productivity_statistics,
-                                         key=lambda unit: unit.orders_per_courier_labour_hour_today,
-                                         reverse=True)
-
-        for unit in units_sorted_statistics:
-            unit_name = self.__unit_uuid_to_name[unit.unit_uuid]
-            lines.append(f'{unit_name} | {unit.orders_per_courier_labour_hour_today}'
-                         f' | {humanize_percents(unit.from_week_before_in_percents)}')
-
+        lines += [
+            f'{unit.unit_name} | {unit.orders_per_courier_labour_hour_today}'
+            f' | {unit.from_week_before_in_percents:+}%'
+            for unit in self.__units
+        ]
         return '\n'.join(lines)
