dbbce755f17605dcbbe25abd5a3b708278639fcc
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 10:29:53 +0600

    :recycle: GI-4 Refactor `awaiting orders` view

--- src/views/statistics_reports/awaiting_orders.py
+++ src/views/statistics_reports/awaiting_orders.py
@@ -1,5 +1,4 @@
-from dodolib import models
-
+import models.report_views.awaiting_orders as models
 from views.base import BaseView
 
 __all__ = ('AwaitingOrdersStatisticsView',)
@@ -8,27 +7,22 @@ __all__ = ('AwaitingOrdersStatisticsView',)
 class AwaitingOrdersStatisticsView(BaseView):
     __slots__ = ('__unit_id_to_name', '__delivery_partial_statistics')
 
-    def __init__(self, delivery_partial_statistics: models.DeliveryPartialStatisticsReport,
-                 unit_id_to_name: dict[int, str]):
-        self.__delivery_partial_statistics = delivery_partial_statistics
-        self.__unit_id_to_name = unit_id_to_name
+    def __init__(self, awaiting_orders_statistics: models.AwaitingOrdersStatisticsReportViewDTO):
+        self.__error_unit_names = awaiting_orders_statistics.error_unit_names
+        self.__units = sorted(
+            awaiting_orders_statistics.units,
+            reverse=True,
+            key=lambda unit: (unit.heated_shelf_orders_count,
+                              unit.couriers_in_queue_count,
+                              unit.couriers_on_shift_count),
+        )
 
     def get_text(self) -> str:
         lines = ['<b>Остывают на полке - В очереди (Всего)</b>']
-        sorted_units_statistics = sorted(
-            self.__delivery_partial_statistics.results,
-            key=lambda unit: (unit.heated_shelf_orders_count, unit.couriers_in_queue_count,
-                              unit.couriers_on_shift_count),
-            reverse=True,
-        )
-
-        for unit in sorted_units_statistics:
-            unit_name = self.__unit_id_to_name[unit.unit_id]
-            lines.append(f'{unit_name} | {unit.heated_shelf_orders_count} - {unit.couriers_in_queue_count}'
-                         f' ({unit.couriers_on_shift_count})')
-
-        for unit_id in self.__delivery_partial_statistics.errors:
-            unit_name = self.__unit_id_to_name[unit_id]
-            lines.append(f'{unit_name} | Ошибка')
-
+        lines += [
+            f'{unit.unit_name} | {unit.heated_shelf_orders_count}'
+            f' - {unit.couriers_in_queue_count} ({unit.couriers_on_shift_count})'
+            for unit in self.__units
+        ]
+        lines += [f'{unit_name} | Ошибка' for unit_name in self.__error_unit_names]
         return '\n'.join(lines)
