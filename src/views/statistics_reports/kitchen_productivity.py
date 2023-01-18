b039db0487fe62e2d956998dbf57d8da72c2bf36
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 10:53:26 +0600

    :recycle: GI-4 Refactor `kitchen productivity` view

--- src/views/statistics_reports/kitchen_productivity.py
+++ src/views/statistics_reports/kitchen_productivity.py
@@ -1,6 +1,5 @@
-from dodolib import models
-
-from services.text_utils import intgaps, humanize_percents
+import models.report_views.kitchen_productivity as models
+from services.text_utils import intgaps
 from views.base import BaseView
 
 __all__ = ('KitchenProductivityStatisticsView',)
@@ -9,20 +8,18 @@ __all__ = ('KitchenProductivityStatisticsView',)
 class KitchenProductivityStatisticsView(BaseView):
     __slots__ = ('__kitchen_productivity_statistics', '__unit_id_to_name')
 
-    def __init__(self, kitchen_productivity_statistics: models.KitchenPartialStatisticsReport,
-                 unit_id_to_name: dict[int, str]):
-        self.__kitchen_productivity_statistics = kitchen_productivity_statistics
-        self.__unit_id_to_name = unit_id_to_name
+    def __init__(self, kitchen_productivity_statistics: models.KitchenProductivityStatisticsReportViewDTO):
+        self.__units = sorted(kitchen_productivity_statistics.units,
+                              key=lambda unit: unit.sales_per_labor_hour_today,
+                              reverse=True)
+        self.__error_unit_names = kitchen_productivity_statistics.error_unit_names
 
     def get_text(self) -> str:
         lines = ['<b>Выручка на чел. в час</b>']
-        sorted_units_statistics = sorted(self.__kitchen_productivity_statistics.results,
-                                         reverse=True,
-                                         key=lambda unit: unit.sales_per_labor_hour_today)
-
-        for unit in sorted_units_statistics:
-            unit_name = self.__unit_id_to_name[unit.unit_id]
-            lines.append(f'{unit_name} | {intgaps(unit.sales_per_labor_hour_today)}'
-                         f' | {humanize_percents(unit.from_week_before_percent)}')
-
+        lines += [
+            f'{unit.unit_name} | {intgaps(unit.sales_per_labor_hour_today)}'
+            f' | {unit.from_week_before_percent:+}%'
+            for unit in self.__units
+        ]
+        lines += [f'{unit_name} | Ошибка' for unit_name in self.__error_unit_names]
         return '\n'.join(lines)
