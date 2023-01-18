3f312c797b46da86adc3e3d5264ccef85b8df356
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 10:30:54 +0600

    :sparkles: GI-4 Add report model to view dto converter

--- src/services/converters.py
+++ src/services/converters.py
@@ -1,9 +1,40 @@
-import models.report_views as view_models
+import collections
+import functools
+from dataclasses import dataclass
+from typing import Iterable
+from uuid import UUID
+
+import models.api_responses.database as database_models
 import models.api_responses.dodo as api_models
+import models.report_views as view_models
 
 __all__ = (
     'to_revenue_statistics_view_dto',
+    'UnitsConverter',
+    'to_awaiting_orders_statistics_view_dto',
 )
+
+
+def to_awaiting_orders_statistics_view_dto(
+        awaiting_orders_statistics_group: Iterable[api_models.AwaitingOrdersStatisticsReport],
+        unit_id_to_name: dict[int, str],
+) -> view_models.AwaitingOrdersStatisticsReportViewDTO:
+    units = []
+    error_unit_names = []
+    for awaiting_orders_statistics in awaiting_orders_statistics_group:
+        units += [
+            view_models.UnitAwaitingOrdersStatisticsDTO(
+                unit_name=unit_id_to_name[unit.unit_id],
+                heated_shelf_orders_count=unit.heated_shelf_orders_count,
+                couriers_in_queue_count=unit.couriers_in_queue_count,
+                couriers_on_shift_count=unit.couriers_on_shift_count,
+            ) for unit in awaiting_orders_statistics.results
+        ]
+        error_unit_names += [unit_id_to_name[unit_id] for unit_id in awaiting_orders_statistics.errors]
+    return view_models.AwaitingOrdersStatisticsReportViewDTO(
+        units=units,
+        error_unit_names=error_unit_names,
+    )
 
 
 def to_revenue_statistics_view_dto(
