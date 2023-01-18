e8d52f2106afeb522308640251c5c5e10c22207f
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 11:46:20 +0600

    :recycle: GI-4 refactor `late delivery vouchers` view

--- src/views/statistics_reports/being_late_certificates.py
+++ src/views/statistics_reports/being_late_certificates.py
@@ -1,8 +1,7 @@
 import uuid
 from typing import Iterable
 
-from dodolib import models
-
+import models.report_views.late_delivery_vouchers as models
 from views.base import BaseView
 
 __all__ = ('BeingLateCertificatesStatisticsView',)
@@ -11,21 +10,17 @@ __all__ = ('BeingLateCertificatesStatisticsView',)
 class BeingLateCertificatesStatisticsView(BaseView):
     __slots__ = ('____units_being_late_certificates_statistics', '__unit_uuid_to_name',)
 
-    def __init__(self, units_being_late_certificates_statistics: Iterable[models.UnitBeingLateCertificates],
-                 unit_uuid_to_name: dict[uuid.UUID, str]):
-        self.__units_being_late_certificates_statistics = units_being_late_certificates_statistics
-        self.__unit_uuid_to_name = unit_uuid_to_name
-
-    def get_text(self) -> str:
-        lines = ['<b>Сертификаты за опоздание (сегодня) | (неделю назад)</b>']
-        sorted_units_statistics = sorted(
-            self.__units_being_late_certificates_statistics,
+    def __init__(self, being_late_certificates_statistics: Iterable[models.UnitLateDeliveryVouchersStatisticsViewDTO]):
+        self.__units = sorted(
+            being_late_certificates_statistics,
             key=lambda unit: (unit.certificates_count_today, unit.certificates_count_week_before),
             reverse=True,
         )
 
-        for unit in sorted_units_statistics:
-            unit_name = self.__unit_uuid_to_name[unit.unit_uuid]
-            lines.append(f'{unit_name} | {unit.certificates_count_today} шт | {unit.certificates_count_week_before} шт')
-
+    def get_text(self) -> str:
+        lines = ['<b>Сертификаты за опоздание (сегодня) | (неделю назад)</b>']
+        lines += [
+            f'{unit.unit_name} | {unit.certificates_count_today} шт | {unit.certificates_count_week_before} шт'
+            for unit in self.__units
+        ]
         return '\n'.join(lines)
