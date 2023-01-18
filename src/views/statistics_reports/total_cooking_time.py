0fd92ad7d969cd146c5229c8e40e64b14f962997
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 19:44:37 +0600

    :recycle: GI-4 Refactor `total cooking time` view

--- src/views/statistics_reports/total_cooking_time.py
+++ src/views/statistics_reports/total_cooking_time.py
@@ -1,5 +1,4 @@
-from dodolib import models
-
+import models.report_views.total_cooking_time as models
 from services.text_utils import humanize_seconds
 from views.base import BaseView
 
@@ -9,18 +8,14 @@ __all__ = ('TotalCookingTimeStatisticsView',)
 class TotalCookingTimeStatisticsView(BaseView):
     __slots__ = ('__kitchen_partial_statistics', '__unit_id_to_name')
 
-    def __init__(self, kitchen_partial_statistics: models.KitchenPartialStatisticsReport,
-                 unit_id_to_name: dict[int, str]):
-        self.__kitchen_partial_statistics = kitchen_partial_statistics
-        self.__unit_id_to_name = unit_id_to_name
+    def __init__(self, total_cooking_time_statistics: models.TotalCookingTimeStatisticsReportViewDTO):
+        self.__units = sorted(total_cooking_time_statistics.units, key=lambda unit: unit.total_cooking_time)
+        self.__error_unit_names = total_cooking_time_statistics.error_unit_names
 
     def get_text(self) -> str:
         lines = ['<b>Общее время приготовления</b>']
-        sorted_units_statistics = sorted(self.__kitchen_partial_statistics.results,
-                                         key=lambda unit: unit.total_cooking_time)
-
-        for unit in sorted_units_statistics:
-            unit_name = self.__unit_id_to_name[unit.unit_id]
-            lines.append(f'{unit_name} | {humanize_seconds(unit.total_cooking_time)}')
-
+        lines += [
+            f'{unit.unit_name} | {humanize_seconds(unit.total_cooking_time)}'
+            for unit in self.__units
+        ]
         return '\n'.join(lines)
