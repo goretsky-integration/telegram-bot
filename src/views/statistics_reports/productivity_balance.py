7bc32664e1e05f646a9b5702cfca8dc4e55add61
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 19:16:48 +0600

    :recycle: GI-4 Refactor `productivity balance` view

--- src/views/statistics_reports/productivity_balance.py
+++ src/views/statistics_reports/productivity_balance.py
@@ -1,7 +1,6 @@
-import uuid
 from typing import Iterable
 
-import models
+import models.report_views.productivity_balance as models
 from services.text_utils import humanize_seconds, intgaps
 from views.base import BaseView
 
@@ -11,20 +10,16 @@ __all__ = ('ProductivityBalanceStatisticsView',)
 class ProductivityBalanceStatisticsView(BaseView):
     __slots__ = ('__units_productivity_statistics', '__unit_uuid_to_name')
 
-    def __init__(self, units_productivity_statistics: Iterable[models.UnitProductivityBalanceStatistics],
-                 unit_uuid_to_name: dict[uuid.UUID, str]):
-        self.__units_productivity_statistics = units_productivity_statistics
-        self.__unit_uuid_to_name = unit_uuid_to_name
+    def __init__(self, productivity_balance_statistics: Iterable[models.UnitProductivityBalanceStatisticsViewDTO]):
+        self.__units = sorted(productivity_balance_statistics,
+                              reverse=True,
+                              key=lambda unit: unit.sales_per_labor_hour)
 
     def get_text(self) -> str:
         lines = ['<b>Баланс эффективности</b>']
-        sorted_units_productivity_statistics = sorted(self.__units_productivity_statistics,
-                                                      reverse=True,
-                                                      key=lambda unit: unit.sales_per_labor_hour)
-
-        for unit in sorted_units_productivity_statistics:
-            unit_name = self.__unit_uuid_to_name[unit.unit_uuid]
-            lines.append(f'{unit_name} | {intgaps(unit.sales_per_labor_hour)} | {unit.orders_per_labor_hour}'
-                         f' | {humanize_seconds(unit.stop_sale_duration_in_seconds)}')
-
+        lines += [
+            f'{unit.unit_name} | {intgaps(unit.sales_per_labor_hour)} | {unit.orders_per_labor_hour}'
+            f' | {humanize_seconds(unit.stop_sale_duration_in_seconds)}'
+            for unit in self.__units
+        ]
         return '\n'.join(lines)
