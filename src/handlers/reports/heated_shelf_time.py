75fbec766410bba472d14939cdfe4000758c43e4
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 20:12:55 +0600

    :recycle: GI-4 Refactor `heated shelf time` handler

--- src/handlers/reports/heated_shelf_time.py
+++ src/handlers/reports/heated_shelf_time.py
@@ -2,17 +2,13 @@ import asyncio
 
 from aiogram import Dispatcher
 from aiogram.dispatcher.filters import Command
-from dodolib import DatabaseClient, AuthClient, DodoAPIClient
-from dodolib.utils.convert_models import UnitsConverter
 
 from models import Query
-from shortcuts import (
-    answer_views,
-    validate_reports,
-    get_message,
-    get_accounts_tokens_batch,
-    get_statistics_report_by_tokens_batch, filter_units_by_ids,
-)
+from services.auth_api import AuthAPIService, get_tokens_batch, get_cookies_batch
+from services.converters import UnitsConverter, to_heated_shelf_time_statistics_view_dto
+from services.database_api import DatabaseAPIService
+from services.dodo_api import DodoAPIService, get_v2_statistics_reports_batch, get_v1_statistics_reports_batch
+from shortcuts import answer_views, get_message, filter_units_by_ids, flatten
 from utils import logger
 from utils.callback_data import show_statistics
 from views import HeatedShelfTimeStatisticsView
@@ -22,27 +18,42 @@ __all__ = ('register_handlers',)
 
 async def on_heated_shelf_time_statistics_report(
         query: Query,
-        api_client: DodoAPIClient,
-        auth_client: AuthClient,
-        db_client: DatabaseClient,
+        dodo_api_service: DodoAPIService,
+        database_api_service: DatabaseAPIService,
+        auth_api_service: AuthAPIService,
 ):
-    logger.debug('New report request')
+    logger.info('Heated shelf time statistics report request')
     message = get_message(query)
-    report_message = await message.answer('Загрузка...')
-    units, reports = await asyncio.gather(
-        db_client.get_units(),
-        db_client.get_reports(chat_id=message.chat.id, report_type='STATISTICS'),
+    report_message, units, reports = await asyncio.gather(
+        message.answer('Загрузка...'),
+        database_api_service.get_units(),
+        database_api_service.get_reports_routes(chat_id=message.chat.id, report_type='STATISTICS'),
     )
-    validate_reports(reports)
     units = UnitsConverter(filter_units_by_ids(units, reports[0].unit_ids))
-    accounts_tokens = await get_accounts_tokens_batch(auth_client, units.account_names)
-    units_statistics = await get_statistics_report_by_tokens_batch(
-        api_client.get_heated_shelf_time_statistics,
-        units.account_names_to_unit_uuids,
-        accounts_tokens,
+    accounts_cookies, accounts_tokens = await asyncio.gather(
+        get_cookies_batch(auth_api_service=auth_api_service, account_names=units.account_names),
+        get_tokens_batch(auth_api_service=auth_api_service, account_names=units.account_names),
     )
-    view = HeatedShelfTimeStatisticsView(units_statistics, units.uuid_to_name())
+    trips_with_one_order_reports, heated_shelf_time_reports = await asyncio.gather(
+        get_v1_statistics_reports_batch(
+            api_method=dodo_api_service.get_trips_with_one_order_statistics_report,
+            units_grouped_by_account_name=units.grouped_by_account_name,
+            accounts_cookies=accounts_cookies,
+        ),
+        get_v2_statistics_reports_batch(
+            api_method=dodo_api_service.get_heated_shelf_time_statistics_report,
+            units_grouped_by_account_name=units.grouped_by_account_name,
+            accounts_tokens=accounts_tokens,
+        )
+    )
+    units_heated_shelf_time_statistics = to_heated_shelf_time_statistics_view_dto(
+        heated_shelf_time_statistics=heated_shelf_time_reports,
+        trips_with_one_order_statistics=flatten(trips_with_one_order_reports),
+        unit_uuid_to_name=units.unit_uuid_to_name,
+    )
+    view = HeatedShelfTimeStatisticsView(units_heated_shelf_time_statistics)
     await answer_views(report_message, view, edit=True)
+    logger.info('Heated shelf time statistics report sent')
 
 
 def register_handlers(dispatcher: Dispatcher):
