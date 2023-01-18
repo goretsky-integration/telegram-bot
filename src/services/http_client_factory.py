51281d1ca2421d4a96868298722d690d3818f974
Eldos <eldos.baktybekov@gmail.com>
2023-01-18 18:27:57 +0600

    :construction: GI-4 Increase http client timeout

--- src/services/http_client_factory.py
+++ src/services/http_client_factory.py
@@ -7,5 +7,5 @@ __all__ = ('closing_http_client_factory',)
 
 @contextlib.asynccontextmanager
 async def closing_http_client_factory(*, base_url: str) -> httpx.AsyncClient:
-    async with httpx.AsyncClient(base_url=base_url) as client:
+    async with httpx.AsyncClient(base_url=base_url, timeout=60) as client:
         yield client
