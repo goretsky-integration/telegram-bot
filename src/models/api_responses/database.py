c9798a1c563b69c70a011291536a41b9ea7f9dee
Eldos <eldos.baktybekov@gmail.com>
2023-03-31 12:13:26 +0600

    :construction: Database API models changes (#GI-51)
    * Add `Region` model.
    * Add `region_id` field in `Unit` model.

--- src/models/api_responses/database.py
+++ src/models/api_responses/database.py
@@ -4,18 +4,22 @@ from pydantic import BaseModel
 
 __all__ = (
     'Unit',
+    'Region',
     'ReportRoute',
     'ReportType',
 )
+
+
+class Region(BaseModel):
+    id: int
+    name: str
 
 
 class Unit(BaseModel):
     id: int
     name: str
     uuid: UUID
-    region: str
-    office_manager_account_name: str
-    dodo_is_api_account_name: str
+    region_id: int
 
 
 class ReportRoute(BaseModel):
