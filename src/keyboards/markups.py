4e519e72a290347a74a20871a80cbb3f41ab2855
Eldos <eldos.baktybekov@gmail.com>
2023-04-01 18:22:59 +0600

    :alien: Refactor views and markups due to using IDs (#GI-51)

--- src/keyboards/markups.py
+++ src/keyboards/markups.py
@@ -1,10 +1,9 @@
-from typing import Iterable
+from collections.abc import Iterable
 
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup
 
 import models.api_responses.database as models
 from keyboards import buttons
-from utils.callback_data import last_n_days_period
 
 __all__ = (
     'UpdateStatisticsReportMarkup',
@@ -12,7 +11,6 @@ __all__ = (
     'RegionsMarkup',
     'SettingsMarkup',
     'UnitsMarkup',
-    'PeriodsMarkup',
 )
 
 
@@ -44,21 +42,21 @@ class SettingsMarkup(InlineKeyboardMarkup):
 
 class RegionsMarkup(InlineKeyboardMarkup):
 
-    def __init__(self, report_type_name: str, regions: Iterable[str]):
-        super().__init__()
+    def __init__(self, report_type_id: int, regions: Iterable[models.Region]):
+        super().__init__(row_width=1)
         self.add(
-            *(buttons.ChooseRegionButton(report_type_name, region) for region in
+            *(buttons.ChooseRegionButton(report_type_id, region) for region in
               regions))
 
 
 class UnitsMarkup(InlineKeyboardMarkup):
 
-    def __init__(self, report_type: str, region: str,
+    def __init__(self, report_type_id: int, region_id: int,
                  enabled_unit_ids: Iterable[int],
                  all_units: Iterable[models.Unit]):
         super().__init__(row_width=2)
-        self.__report_type = report_type
-        self.__region = region
+        self.__report_type_id = report_type_id
+        self.__region_id = region_id
         self.__all_units = all_units
         self.__enabled_unit_ids = set(enabled_unit_ids)
 
@@ -76,38 +74,26 @@ class UnitsMarkup(InlineKeyboardMarkup):
     def __add_unit_operations(self) -> None:
         for unit in self.__all_units:
             is_unit_enabled = unit.id in self.__enabled_unit_ids
-            button = buttons.SwitchUnitStatusButton(self.__report_type,
-                                                    self.__region,
-                                                    unit.id, unit.name,
-                                                    is_unit_enabled)
+            button = buttons.SwitchUnitStatusButton(
+                report_type_id=self.__report_type_id,
+                region_id=self.__region_id,
+                unit_id=unit.id,
+                unit_name=unit.name,
+                is_unit_enabled=is_unit_enabled,
+            )
             self.insert(button)
 
     def __add_batch_operations(self) -> None:
         row = []
         if any(self.__disabled_unit_ids):
-            row.append(buttons.EnableAllUnitsByRegionButton(self.__report_type,
-                                                            self.__region))
+            row.append(buttons.EnableAllUnitsByRegionButton(
+                report_type_id=self.__report_type_id,
+                region_id=self.__region_id,
+            ))
         if any(self.__enabled_unit_ids):
-            row.append(buttons.DisableAllUnitsByRegionButton(self.__report_type,
-                                                             self.__region))
+            row.append(buttons.DisableAllUnitsByRegionButton(
+                report_type_id=self.__report_type_id,
+                region_id=self.__region_id,
+            ))
         self.row(*row)
 
-
-class PeriodsMarkup(InlineKeyboardMarkup):
-
-    def __init__(self):
-        super().__init__(row_width=1)
-        self.add(
-            InlineKeyboardButton('последние 7 дней',
-                                 callback_data=last_n_days_period.new(
-                                     days_before_count=7)),
-            InlineKeyboardButton('последние 14 дней',
-                                 callback_data=last_n_days_period.new(
-                                     days_before_count=14)),
-            InlineKeyboardButton('последние 30 дней',
-                                 callback_data=last_n_days_period.new(
-                                     days_before_count=30)),
-            InlineKeyboardButton('последние 60 дней',
-                                 callback_data=last_n_days_period.new(
-                                     days_before_count=60)),
-        )
--- src/views/settings.py
+++ src/views/settings.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from collections.abc import Iterable
 
 from aiogram.types import InlineKeyboardMarkup
 
@@ -15,28 +15,34 @@ __all__ = (
 
 class RegionsResponseView(BaseView):
 
-    def __init__(self, report_type: str, regions: Iterable[str]):
+    def __init__(self, report_type_id: int, regions: Iterable[models.Region]):
         self.__regions = regions
-        self.__report_type = report_type
+        self.__report_type_id = report_type_id
 
     def get_text(self) -> str:
         return 'Выберите регион подразделений'
 
     def get_reply_markup(self) -> InlineKeyboardMarkup:
-        return RegionsMarkup(self.__report_type, self.__regions)
+        return RegionsMarkup(self.__report_type_id, self.__regions)
 
 
 class UnitsResponseView(BaseView):
 
-    def __init__(self, report_type: str, region: str,
-                 enabled_unit_ids: Iterable[int], all_units: Iterable[models.Unit]):
-        self.__report_type = report_type
+    def __init__(self, report_type_id: int, region_id: int,
+                 enabled_unit_ids: Iterable[int],
+                 all_units: Iterable[models.Unit]):
+        self.__report_type_id = report_type_id
         self.__enabled_unit_ids = enabled_unit_ids
         self.__all_units = all_units
-        self.__region = region
+        self.__region_id = region_id
 
     def get_text(self) -> str:
         return 'Выберите точки продаж для отслеживания'
 
     def get_reply_markup(self) -> InlineKeyboardMarkup:
-        return UnitsMarkup(self.__report_type, self.__region, self.__enabled_unit_ids, self.__all_units)
+        return UnitsMarkup(
+            report_type_id=self.__report_type_id,
+            region_id=self.__region_id,
+            enabled_unit_ids=self.__enabled_unit_ids,
+            all_units=self.__all_units,
+        )
