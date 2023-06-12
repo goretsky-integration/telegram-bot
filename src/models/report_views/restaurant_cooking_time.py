1daf37ddb86ad8b548130e45682814789dd7333a
Eldos <eldos.baktybekov@gmail.com>
2023-06-12 11:46:41 +0600

    :fire: Remove model (#GI-66)

--- src/models/report_views/restaurant_cooking_time.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-__all__ = ('UnitRestaurantCookingTimeStatisticsViewDTO',)
-
-
-@dataclass(frozen=True, slots=True)
-class UnitRestaurantCookingTimeStatisticsViewDTO:
-    unit_name: str
-    average_tracking_pending_and_cooking_time: int
